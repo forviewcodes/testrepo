a70dc4c0bbc42bd0f9023892ed8dab9875b74a6e
Harris <harris@Harriss-MacBook-Pro.local>
2025-03-07 10:17:02 +0800

    chore: add theme hook/component/icons + twmerge in lib/utils

---
+++ Icons/index.tsx
@@ -0,0 +1,7 @@
+export * from "./MonochromeIcon";
+export * from "./MoonIcon";
+export * from "./SepiaIcon";
+export * from "./SunIcon";
+export * from "./PlaceholderIcon";
+export * from "./Bars3Icon";
+export * from "./XMarkIcon";
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { ThemeToggler } from "@/components/ThemeToggler";
+import { MonochromeIcon, MoonIcon, SepiaIcon, SunIcon } from "@/Icons";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <ThemeToggler
+        themes={[
+          { theme: "light", icon: <SunIcon /> },
+          { theme: "dark", icon: <MoonIcon /> },
+          { theme: "sepia", icon: <SepiaIcon /> },
+          { theme: "monochrome", icon: <MonochromeIcon /> },
+        ]}
+      ></ThemeToggler>
+    </div>
+  );
 }
