fb38359cc596e0cdd3246f22276c2a89ff75c328
Harris Azmi <azmi.harris@gmail.com>
2025-03-10 00:03:28 +0800

    chore:add tools

--- data/ProjectsData.ts
+++ data/ProjectsData.ts
@@ -89,3 +89,58 @@ export const collabProjectsData = [
     previewUrl: "https://project1frontendcf.harrisviewcodes.uk/",
   },
 ];
+
+export const ToolsImages = [
+  { name: "C++", path: "/tools/languages/1.png", type: "languages" },
+  { name: "Javascript", path: "/tools/languages/2.png", type: "languages" },
+  { name: "Python", path: "/tools/languages/3.png", type: "languages" },
+
+  //takda lagi image
+  { name: "TypeScript", path: "/tools/languages/4.png", type: "languages" },
+  //
+
+  { name: "NextJs", path: "/tools/fullstack/1.png", type: "fullstack" },
+  { name: "HTML5", path: "/tools/frontend/1.png", type: "frontend" },
+  { name: "CSS3", path: "/tools/frontend/2.png", type: "frontend" },
+  { name: "TailwindCSS", path: "/tools/frontend/3.png", type: "frontend" },
+  { name: "React", path: "/tools/frontend/4.png", type: "frontend" },
+
+  //takda lagi image
+  { name: "Vite", path: "/tools/frontend/5.png", type: "frontend" },
+  //
+
+  { name: "Node.js", path: "/tools/backend/1.png", type: "backend" },
+  { name: "Express.js", path: "/tools/backend/2.png", type: "backend" },
+  { name: "Mongoose", path: "/tools/backend/3.png", type: "backend" },
+  { name: "jQuery", path: "/tools/backend/4.png", type: "backend" },
+  { name: "GraphQL", path: "/tools/backend/5.png", type: "backend" },
+  { name: "MongoDB", path: "/tools/database/1.png", type: "database" },
+  { name: "MySQL", path: "/tools/database/2.png", type: "database" },
+  { name: "Oracle", path: "/tools/database/3.png", type: "database" },
+  { name: "SQL Server", path: "/tools/database/4.png", type: "database" },
+  { name: "PostgreSQL", path: "/tools/database/5.png", type: "database" },
+  { name: "Docker", path: "/tools/devops/1.png", type: "devops" },
+  { name: "DockerHub", path: "/tools/devops/2.png", type: "devops" },
+  {
+    name: "Linux Container (LXC)",
+    path: "/tools/devops/3.png",
+    type: "devops",
+  },
+  { name: "Portainer", path: "/tools/devops/4.png", type: "devops" },
+  { name: "Proxmox", path: "/tools/devops/5.png", type: "devops" },
+  { name: "ESXI", path: "/tools/devops/7.png", type: "devops" },
+  { name: "CentOs", path: "/tools/devops/8.png", type: "devops" },
+  { name: "Ubuntu LTS", path: "/tools/devops/9.png", type: "devops" },
+  { name: "Nginx", path: "/tools/devops/10.png", type: "devops" },
+  { name: "Cloudflare", path: "/tools/devops/11.png", type: "devops" },
+  { name: "Terraform", path: "/tools/devops/12.png", type: "devops" },
+
+  //takda lagi image
+  { name: "AWS", path: "/tools/devops/13.png", type: "devops" },
+  //
+
+  { name: "Postman", path: "/tools/others/1.png", type: "others" },
+  { name: "Git", path: "/tools/others/2.png", type: "others" },
+  { name: "GitHub", path: "/tools/others/3.png", type: "others" },
+  { name: "Jest", path: "/tools/others/4.png", type: "others" },
+];
