3cee583ebe12d2f770bc1f323f6c165a764ab482
Harris Azmi <azmi.harris@gmail.com>
2025-03-29 16:07:07 +0800

    chore:added resume and avatar images

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,50 +1,64 @@
-import { GitHubIcon, LinkedInIcon, MailIcon, ResumeIcon } from "@/Icons";
+"use client";
 import { Button } from "./Button";
 import { IconAnimation } from "./IconAnimation";
 import { clx } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { IconInfo } from "./IconInfo";
+import { AvatarInfo } from "@/data/AvatarInfo";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Avatar() {
-  const tabs = [
-    { href: "https://www.github.com", label: "Github", icon: <GitHubIcon /> },
-    {
-      href: "https://www.linkedin.com",
-      label: "LinkedIn",
-      icon: <LinkedInIcon />,
-    },
-    {
-      href: "mailto:test@gmail.com?subject=Hello&body=How%20are%20you?",
-      label: "Mail",
-      icon: <MailIcon />,
-    },
-    { href: "/experiences", label: "Resume", icon: <ResumeIcon /> },
-  ];
-
   return (
     <div className="h-[640px] w-[345px] bg-gray-400 rounded-xl flex flex-col justify-between p-8 sticky top-10">
       <div className="items-center justify-center flex flex-col">
         <div className="h-[280px] w-[240px]  bg-black rounded-lg mb-6 overflow-clip">
           <Image
-            src={"/avatar/avatar.png"}
+            src={"/avatar/avatar-light.png"}
             alt={"avatar"}
             width={240}
             height={280}
           ></Image>
+          {/* <Image
+            className="img-dark"
+            src={"/avatar/avatar-dark.png"}
+            alt={"avatar"}
+            width={240}
+            height={280}
+          ></Image> */}
         </div>
         <div className="flex flex-col items-start space-y-1 text-gray-900 dark:text-white">
           <div className="text-center text-black">
             <h1 className="text-4xl font-extrabold">Harris Azmi</h1>
             <h2 className="text-xl font-medium">
-              Fullstack Developer & Frontend Specialist
+              <TypeAnimation
+                sequence={[
+                  "FullStack Web Developer",
+                  1000,
+                  "Frontend Specialist",
+                  1000,
+                  "Dev Ops Engineer",
+                  1000,
+                  "Home Lab Brewer",
+                  1000,
+                  "MERN Stack Developer",
+                  1000,
+                  "Self-hosted Infra Engineer",
+                  1000,
+                  "Chemical Process Engineer",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
             </h2>
             <p className="text-lg">Shah Alam, Malaysia</p>
           </div>
         </div>
 
         <div className="space-x-4 py-4 flex">
-          {tabs.map((tab) => (
+          {AvatarInfo.map((tab) => (
             <Link
               key={tab.href}
               href={tab.href}
@@ -62,7 +76,11 @@ export default function Avatar() {
         </div>
       </div>
       <div className="items-center justify-center flex">
-        <Button className="px-10 py-2">Let's talk</Button>
+        <Link href={"/contacts"}>
+          <Button className="px-10 py-2 hover:cursor-pointer">
+            Let's talk
+          </Button>
+        </Link>
       </div>
     </div>
   );
