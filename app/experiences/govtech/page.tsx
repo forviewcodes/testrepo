0a49f59c2e742ba91600df19f9fa392b512cb978
Harris Azmi <azmi.harris@gmail.com>
2025-04-07 09:02:25 +0800

    chore:add experience

--- app/experiences/govtech/page.tsx
+++ app/experiences/govtech/page.tsx
@@ -1,32 +1,120 @@
+"use client";
 import { Button } from "@/components/Button";
-import { ArrowLeftIcon, BroadcastIcon, GitHubIcon } from "@/Icons";
+import { ExpGovTech, ExpInfo } from "@/data/ExpInfo";
+import { ArrowLeftIcon, HomeIcon } from "@/Icons";
+import ReactMarkdown from "react-markdown";
+import { motion, useInView } from "framer-motion";
 import Link from "next/link";
+import { useRef } from "react";
+import { usePathname } from "next/navigation";
 
 export default function GovtechPage() {
+  const pathname = usePathname();
+  const modifiedPathname = pathname.replace("/experiences/", "");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
   return (
-    <div>
+    <div className="pb-4">
       <Link href="/experiences">
         <Button className="flex gap-4">
           <ArrowLeftIcon />
           All Experiences
         </Button>
       </Link>
+      <div className="py-8 flex flex-col gap-8">
+        <div>
+          <h1 className="font-bold text-5xl pb-1">{ExpGovTech.name}</h1>
+          <p>{ExpGovTech.year}</p>
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl pb-4">Overview</h2>
+          <p>{ExpGovTech.overview}</p>
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl pb-4">My Role</h2>
+          <p>{ExpGovTech.myRole}</p>
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl pb-6">
+            Skills Acquired and Contributions
+          </h2>
+          <div className="space-y-4">
+            {ExpGovTech.keyAchievementsAndContributions.map(
+              (achievement, index) => (
+                <div key={index} className="">
+                  <h3 className="font-semibold text-xl">
+                    {achievement.heading}
+                  </h3>
+                  <ul>
+                    {achievement.details.map((detail, detailIndex) => (
+                      <div key={detailIndex}>
+                        <ReactMarkdown
+                          components={{
+                            p: ({ children }) => <>{children}</>,
+                            strong: ({ children }) => (
+                              <strong className="font-bold">{children}</strong>
+                            ),
+                          }}
+                        >
+                          {detail}
+                        </ReactMarkdown>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+              )
+            )}
+          </div>
+        </div>
 
-      <h1 className="font-bold text-5xl">GovTech</h1>
+        <div>
+          <h2 className="font-bold text-4xl pb-4">Impact</h2>
+          <p>{ExpGovTech.impact}</p>
+        </div>
 
-      <p>Jan - Feb 2024</p>
-      <div>Overview</div>
-      <p>Heheheh</p>
-      <div>My Role</div>
-      <p>Hehehe</p>
-      <div>Skills Acquired</div>
-      <p>hehehe</p>
-      <div>Impact</div>
-      <p>hehe</p>
+        <Link
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="px-8 py-2">Company Link</Button>
+        </Link>
+      </div>
 
-      <div>References</div>
-      <p>PIC name</p>
-      <p>PIC email</p>
+      <h1 className="font-bold text-5xl pb-6">Previous Experiences</h1>
+      <motion.div
+        variants={cardVariants}
+        initial="initial"
+        animate={isInView ? "animate" : "initial"}
+        transition={{ duration: 0.3, delay: 0.2 }}
+      >
+        <div ref={ref} className="gap-4 flex flex-col">
+          {ExpInfo.map((exp, index) => {
+            if (modifiedPathname === exp.name.toLowerCase()) {
+              return null;
+            }
+
+            return (
+              <Link key={index} href={exp.href}>
+                <div className="bg-gray-400 h-auto rounded-2xl flex justify-between p-6">
+                  <div>
+                    <div className="text-4xl font-semibold">{exp.name}</div>
+                    <div className="py-4 text-lg">{exp.details}</div>
+                    <div>{exp.year}</div>
+                  </div>
+                  <HomeIcon className="shrink-0" />
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </motion.div>
     </div>
   );
 }
---
+++ data/ExpInfo.tsx
@@ -0,0 +1,109 @@
+export const ExpInfo = [
+  {
+    name: "GovTech",
+    details:
+      "Full-Stack Engineer at GNU, responsible for developing, deploying, and maintaining scalable web and mobile applications for the public sector. Focused on creating industry-standard solutions to improve accessibility, efficiency, and user engagement in public services. Collaborated with cross-functional teams to drive innovation and ensure the delivery of high-quality software solutions.",
+    year: "May 2024 - Present",
+    href: "/experiences/govtech",
+  },
+  {
+    name: "Bateriku",
+    details:
+      "As a Software Engineer/Developer at Bateriku (M) Sdn Bhd, I was responsible for maintaining backend systems using Ruby on Rails, ensuring high performance and reliability. My active participation in the full development lifecycle, from planning to deployment, helped streamline workflows and accelerate delivery timelines.",
+    year: "Jan 2024 - May 2024",
+    href: "/experiences/bateriku",
+  },
+  {
+    name: "Chemiz",
+    details:
+      "Production Manager at Chemiz (M) Sdn Bhd, leading the development and implementation of ISO 9001, ISO 19001, and ISO 13485 standards to ensure compliance with international quality management practices. Spearheaded the integration of cloud-based storage solutions, improving data management and reducing reliance on physical documentation. Directed production teams in maintaining the highest quality assurance standards, while driving cost-effective operational improvements and leading new product development initiatives in Lab (Formulations) operations.",
+    year: "June 2022 - Jan 2024",
+    href: "/experiences/chemiz",
+  },
+];
+
+export const ExpGovTech = {
+  name: "GovTech Nucleus Unit",
+  year: "May 2024 - Present",
+  overview:
+    "As part of Malaysia’s flagship digital transformation initiative under the MyDIGITAL blueprint, I contribute to a high-impact team within the GovTech Nucleus Unit—an elite, in-house tech force driving end-to-end modernization of public services. Our mission is to build scalable, cloud-native web and mobile applications that tackle real public sector challenges and deliver seamless, citizen-first digital experiences. By leveraging AI, big data, and modern infrastructure, we’re transforming government services to be faster, clearer, and easier to access for all Malaysians.",
+  myRole:
+    "As a Full Stack Engineer, my role involves end-to-end development of applications, from designing system architecture to deploying production-ready code. Responsibilities span across frontend interfaces using frameworks like Next.js and backend services powered by technologies such as Node.js. The role requires close collaboration with UI/UX designers, product owners, and fellow engineers to build thoughtful, performant solutions. Additionally, it includes managing deployments across both cloud and on-premises environments.",
+  keyAchievementsAndContributions: [
+    {
+      heading: "Platform Development",
+      details: [
+        "Led and Contributed on developing a centralized platform, **AskGovMy**, mainly on frontend that consolidates FAQs from multiple government agencies, enhancing citizen-government communication and accessibility.",
+      ],
+    },
+    {
+      heading: "Directory Structure",
+      details: [
+        "Contributed on  structured **directory** for seamless information retrieval and navigation with proper CI/CD pipelines.",
+      ],
+    },
+    {
+      heading: "Design System Contribution",
+      details: [
+        "Contributed to the development of **MYDS**, a design system ensuring UI/UX consistency across government-related applications.",
+      ],
+    },
+    {
+      heading: "Frontend Development",
+      details: [
+        "Deeper proficiency in **Next.js**, **React**, **TypeScript**, and **Tailwind CSS** for building scalable and performant web applications.",
+        "Led frontend development initiatives, coordinating with cross-functional teams to drive project success.",
+      ],
+    },
+    {
+      heading: "Search & Data Management",
+      details: [
+        "Experienced in **Elasticsearch** for efficient search implementation.",
+        "Utilized **AWS SDK (S3, SES)** for cloud-based storage and email services.",
+      ],
+    },
+    {
+      heading: "DevOps & CI/CD",
+      details: [
+        "Acquired extensive experience in **cloud deployment** and **infrastructure management** using **AWS**, **Docker**, and **Kubernetes**.",
+        "Focused on establishing and refining **continuous integration** and **delivery (CI/CD)** pipelines.",
+      ],
+    },
+    {
+      heading: "Software Development Best Practices",
+      details: [
+        "Specialized in **modular architecture**, **multilingual support**, **accessibility compliance**, and **performance optimization** for enterprise applications.",
+      ],
+    },
+    {
+      heading: "Project Management",
+      details: [
+        "Developed a strong ability to collaborate effectively within cross-functional teams.",
+        "Utilized **Agile methodologies** and tools such as **Linear** and **GitHub Kanban** for efficient project management and delivery.",
+      ],
+    },
+  ],
+  impact:
+    "My work at the Nucleus Unit has contributed directly to improving public access to government services, reducing friction for citizens and enabling greater transparency. I’ve played a key role in developing digital platforms that are now serving thousands of users, while also helping to set best practices for scalable, secure engineering in the public sector.",
+};
+
+export const ExpBateriku = {
+  name: "GovTech Nucleus Unit",
+  year: "May 2024 - Present",
+  overview: " at HEHEHE",
+  myRole: "fullstack engineer",
+  skillsAquired:
+    "Advanced proficiency in design tools like Adobe XD, Sketch, and Figma. Deepened understanding of user-centered design principles and usability testing. Enhanced ability to work in a fast-paced, creative environment with tight deadlines.",
+  impact:
+    "‍My experience at PixelForge Studios significantly shaped my design philosophy, emphasizing the importance of blending creativity with functionality. The projects I worked on not only expanded my skill set but also instilled a passion for creating designs that resonate with users on a deeper level.",
+};
+
+export const ExpChemiz = {
+  name: "GovTech Nucleus Unit",
+  year: "May 2024 - Present",
+  overview: " at HEHEHE",
+  myRole: "fullstack engineer",
+  skillsAquired:
+    "advancing in fullstack development, cloud computing, and agile methodologies.",
+  impact: "",
+};
